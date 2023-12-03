2351721b3dd779b11f2a95d7e9a07aab7258d150
ahmed-gaber-elgamal <yakan44444@gmail.com>
2023-12-04 01:06:59 +0200

    [UPD]permission_request:update access

--- permission_request/__manifest__.py
+++ permission_request/__manifest__.py
@@ -24,6 +24,7 @@
 
     # always loaded
     'data': [
+        'security/groups.xml',
         'security/ir.model.access.csv',
         'views/permission_request.xml',
         'reports/permission_request.xml',
