1d8f577ac761a11c4134728a340ffe6f55a06eb6
ahmed-gaber-elgamal <yakan44444@gmail.com>
2024-02-08 00:45:20 +0200

    add active fields

--- permission_request/models/permission_request.py
+++ permission_request/models/permission_request.py
@@ -26,6 +26,7 @@ class PermissionRequest(models.Model):
     barcode = fields.Char(default=generate_barcode)
     qr_image = fields.Binary("QR Code", compute='_generate_qr_code')
     qr_url = fields.Char("QR Code", compute='_generate_qr_code')
+    active = fields.Boolean(default=True)
 
     def _generate_qr_code(self):
         for rec in self:
