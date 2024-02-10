9a5b9c0a1dd0eeebb7b9abc0d961672a64404337
ahmed-gaber-elgamal <yakan44444@gmail.com>
2024-02-10 23:18:44 +0200

    [UPD]care_stock:update delivery slip report

--- care_stock/models/stock_warehouse.py
+++ care_stock/models/stock_warehouse.py
@@ -4,4 +4,4 @@ from odoo import fields, models, api
 class StockWarehouse(models.Model):
     _inherit = 'stock.warehouse'
 
-    warehouse_address = fields.Char()
+    warehouse_address = fields.Html()
