19cb929c236b810be3b1bd1794ed42fff5ef652f
Omar-Gad <omargad9000@gmail.com>
2024-12-10 15:45:34 +0200

    [MIG]eg_sale_&_po_line_qoh: upgrade module to v17.0

--- eg_po_line_qoh/__manifest__.py
+++ eg_po_line_qoh/__manifest__.py
@@ -1,20 +1,17 @@
 {
     "name": "On Hand Quantity in Purchase Order",
-    "summary": "Display on hand quantity and forecasted qty in purchase order line.",
     "version": "17.0",
     "category": 'Purchase',
+    "summary": "Display on hand quantity and forecasted qty in purchase order line.",
     'author': 'INKERP',
     'website': "https://www.INKERP.com",
-    "description": "This app will use to display on hand quantity and forecasted qty in purchase order line.",
-    "depends": [
-        'purchase',
-    ],
-    
+    "depends": ['purchase'],
+
     "data": [
         'views/purchase_order_view.xml',
         'report/purchase_order_report.xml'
     ],
-    
+
     'images': ['static/description/banner.png'],
     'license': "OPL-1",
     'installable': True,
--- eg_sale_line_qoh/__manifest__.py
+++ eg_sale_line_qoh/__manifest__.py
@@ -1,20 +1,18 @@
 {
-    "name": "On hand Qty on Sale Order",
+    "name": "On Hand Qty on Sale Order",
     "summary": "Display On hand quantity and forecasted qty in sale order line.",
     "version": "17.0",
     "category": "Sales",
     "description": "Display On hand quantity and forecasted qty in sale order line.",
     "author": "INKERP",
     "website": "https://www.INKERP.com",
-    "depends": [
-        'sale_management',
-    ],
-    
+    "depends": ['sale_management'],
+        
     "data": [
         'views/sale_order_line_view.xml',
-        # 'report/sale_order_report.xml'
+        'report/sale_order_report.xml'
     ],
-    
+
     'images': ['static/description/banner.png'],
     'license': "OPL-1",
     'installable': True,
