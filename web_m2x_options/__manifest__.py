4ff73c370edf139e4a08e98ba044f4c48dcc3f86
Omar-Gad <omargad9000@gmail.com>
2024-12-14 00:05:33 +0200

    [MIG]web_m2x_options: upgrade module to v17.0

--- web_m2x_options/__manifest__.py
+++ web_m2x_options/__manifest__.py
@@ -6,22 +6,26 @@
 
 {
     "name": "web_m2x_options",
-    "version": "17.0",
+    "version": "17.0.1.0.0",
     "category": "Web",
     "author": "initOS GmbH,"
     "ACSONE SA/NV, "
     "0k.io, "
     "Tecnativa, "
+    "Sygel, "
     "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/web",
     "license": "AGPL-3",
     "depends": ["web"],
     "assets": {
         "web.assets_backend": [
-            "web_m2x_options/static/src/js/form.js",
-            "web_m2x_options/static/src/js/ir_options.js",
-        ],
-        "web.assets_qweb": ["web_m2x_options/static/src/xml/base.xml"],
+            (
+                "before",
+                "web/static/src/views/fields/*",
+                "web_m2x_options/static/src/components/form.esm.js",
+            ),
+            "web_m2x_options/static/src/components/base.xml",
+        ]
     },
     "installable": True,
 }
