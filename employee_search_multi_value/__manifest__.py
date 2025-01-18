73a3f916f3b1baea33165cafbea8fe4cea130f97
ahmed-gaber-elgamal <yakan44444@gmail.com>
2025-01-18 17:32:46 +0200

    add employee_search_multi_value/

---
+++ employee_search_multi_value/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    "name": "Employee Search Multi Value",
+    "version": "17.0.1.0.1",
+    "license": "AGPL-3",
+    "depends": ["hr"],
+    "data": ["data/search_field_data.xml", "views/hr_view.xml"],
+    "demo": [],
+}
