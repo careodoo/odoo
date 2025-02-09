389dbc8b52e03d93f49df4c94c23cdd4bd2564b3
Merna <94222075+Mernahmedd@users.noreply.github.com>
2025-02-09 02:01:21 +0200

    [UPD]care_invoice_report:add regular template with and without header, add en template with and without header and arabic template with and without header

--- care_invoice_report/models/account_move.py
+++ care_invoice_report/models/account_move.py
@@ -40,7 +40,35 @@ class AccountMove(models.Model):
                         amt_value=_num2words(fractional_value, lang=lang.iso_code),
                         amt_word="فلس",
                         )
-        return amount_words + ' فقط لا غير '
+        return ' فقط ' + amount_words + ' فقط لا غير '
+    
+    def number_to_english(self, amount):
+        self.ensure_one()
+        def _num2words(number, lang):
+            try:
+                return num2words(number, lang=lang).title()
+            except NotImplementedError:
+                return num2words(number, lang='en').title()
+        if num2words is None:
+            logging.getLogger(__name__).warning("The library 'num2words' is missing, cannot render textual amounts.")
+            return ""
+
+        formatted = "%.{0}f".format(self.currency_id.decimal_places) % amount
+        parts = formatted.partition('.')
+        integer_value = int(parts[0])
+        fractional_value = int(parts[2] or 0)
+
+        lang = self.env['res.lang'].search([('code', '=', 'en')])
+        amount_words = tools.ustr('{amt_value} {amt_word}').format(
+                        amt_value=_num2words(integer_value, lang=lang.iso_code),
+                        amt_word=self.currency_id.currency_unit_label or self.currency_id.name,
+                        )
+        if not self.currency_id.is_zero(amount - integer_value):
+            amount_words += ' ' + _('and') + tools.ustr(' {amt_value} {amt_word}').format(
+                        amt_value=_num2words(fractional_value, lang=lang.iso_code),
+                        amt_word=self.currency_id.currency_subunit_label or 'Cents',
+                        )
+        return ' Only ' + amount_words + ' Only '
 
     def get_invoice_portal_url(self):
         base_url = self.env['ir.config_parameter'].get_param('web.base.url')
